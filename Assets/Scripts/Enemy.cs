50ee8a7eb26162df10d61d9b13f8641a597de1d2
Pepice23 <mojo12984@gmail.com>
2020-02-19 00:50:03 +0100

    added collision

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    //Enemy speed
+    [SerializeField]
+    private float _speed = 4.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+        if (transform.position.y < -5.5f)
+        {
+            float randomPosX = Random.Range(-8, 8);
+            transform.position = new Vector3(randomPosX, 7, 0);
+        }
+
+    }
+    void CalculateMovement() 
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            
+            Destroy(this.gameObject);
+        }
+
+        if (other.CompareTag("Laser"))
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
